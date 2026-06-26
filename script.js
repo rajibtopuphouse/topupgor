ca5f818e6f8aafd182eb35413634680e18d7ed61
rajibtopuphouse <rstopuphouse@gmail.com>
2026-06-26 18:52:30 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+function login() {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
 
+  if (email === "" || password === "") {
+    alert("অনুগ্রহ করে ইমেইল ও পাসওয়ার্ড লিখুন।");
+    return;
+  }
+
+  alert("Login সফল হয়েছে (ডেমো)।");
+  window.location.href = "index.html";
+}
+
+function googleLogin() {
+  alert("Google Login ফিচার যোগ করতে Firebase সেটআপ করতে হবে।");
+}
