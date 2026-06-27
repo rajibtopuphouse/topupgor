66f85cfcd995223e6004d0c859794fb2b5cf074c
rajibtopuphouse <rstopuphouse@gmail.com>
2026-06-27 16:42:16 +0600

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,13 @@
+// Firebase Configuration
+
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT.appspot.com",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
+
+// এখানে পরে Firebase SDK যোগ করা হবে
+console.log("Firebase Ready");
